142862a4aafb6302eae525e9658a51f64d2d418c
WonJunmyung <helpbab@gmail.com>
2023-09-20 14:21:30 +0900

    20230920

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -6,7 +6,14 @@ namespace Silly
 {
     public enum BlockColorName
     {
-        ColorWall,
+        Wall,
+        None,
+        Path,
+        Response,
+        BeforeBuild,
+        Build,
+        ProtecedBuild,
+        FinalProtecedBuild
 
     }
 
@@ -14,11 +21,10 @@ namespace Silly
         public BlockColorName blockColorName;
         public int x;
         public int y;
-
-
     }
     public class MapManager : MonoBehaviour
     {
+        List<MapData> mapDatas = new List<MapData>();
         public Texture2D MapInfo;
         public Color[] ColorWall;
 
@@ -26,14 +32,13 @@ namespace Silly
         private int mapHeight;
 
         public GameObject[] Block;
+        public Transform Map;
         
-        
-
 
         // Start is called before the first frame update
         void Start()
         {
-        
+            GenerateMap();
         }
 
         // Update is called once per frame
@@ -57,11 +62,40 @@ namespace Silly
                 {
                     Color pixelColor = pixels[i * mapHeight + j];
                     Debug.Log(pixelColor);
-
-                    if(pixelColor == ColorWall[(int)BlockColorName.ColorWall])
+                    
+                    if (pixelColor == ColorWall[(int)BlockColorName.Wall])
                     {
-
+                        Instantiate(Block[(int)BlockColorName.Wall], new Vector3(i,0,j), Block[(int)BlockColorName.Wall].transform.rotation, Map);
                     }
+                    else if (pixelColor == ColorWall[(int)BlockColorName.BeforeBuild])
+                    {
+                        Instantiate(Block[(int)BlockColorName.BeforeBuild], new Vector3(i, 0, j), Block[(int)BlockColorName.BeforeBuild].transform.rotation, Map);
+                    }
+                    else if (pixelColor == ColorWall[(int)BlockColorName.Build])
+                    {
+                        Instantiate(Block[(int)BlockColorName.Build], new Vector3(i, 0, j), Block[(int)BlockColorName.Build].transform.rotation, Map);
+                    }
+                    else if (pixelColor == ColorWall[(int)BlockColorName.FinalProtecedBuild])
+                    {
+                        Instantiate(Block[(int)BlockColorName.FinalProtecedBuild], new Vector3(i, 0, j), Block[(int)BlockColorName.FinalProtecedBuild].transform.rotation, Map);
+                    }
+                    else if (pixelColor == ColorWall[(int)BlockColorName.None])
+                    {
+                        Instantiate(Block[(int)BlockColorName.None], new Vector3(i, 0, j), Block[(int)BlockColorName.None].transform.rotation, Map);
+                    }
+                    else if (pixelColor == ColorWall[(int)BlockColorName.Path])
+                    {
+                        Instantiate(Block[(int)BlockColorName.Path], new Vector3(i, 0, j), Block[(int)BlockColorName.Path].transform.rotation, Map);
+                    }
+                    else if (pixelColor == ColorWall[(int)BlockColorName.ProtecedBuild])
+                    {
+                        Instantiate(Block[(int)BlockColorName.ProtecedBuild], new Vector3(i, 0, j), Block[(int)BlockColorName.ProtecedBuild].transform.rotation, Map);
+                    }
+                    else if (pixelColor == ColorWall[(int)BlockColorName.Response])
+                    {
+                        Instantiate(Block[(int)BlockColorName.Response], new Vector3(i, 0, j), Block[(int)BlockColorName.Response].transform.rotation, Map);
+                    }
+                    
                 }
             }
         }
